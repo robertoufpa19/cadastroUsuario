9a18a2b01fee60614e62f6dc2f52bc8fadd211b1
Roberto Oliveira <robertoufpa19@gmail.com>
2023-06-19 14:14:30 -0300

    cadastro simples

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int idUsuario;
+     char nomeUsuario [20];
+
+
+
+         printf("Digite o nome do usuario: ");
+         scanf("%s",&nomeUsuario);
+
+         printf("Nome do usuario Cadastrado: %s ",nomeUsuario);
+
+
+         for(idUsuario=0; idUsuario<6; idUsuario++){
+
+            return idUsuario++;
+         }
+
+         printf("%i",idUsuario);
+
+
+
+}
